660eec4f9cbc43b91700dc2cd2bf517c85cd6754
Flynn OConnell <flynnoconnell@gmail.com>
2025-07-14 12:34:18 -0400

    update author, hpc

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "MBO Compute Hub"
 copyright = "2024, Elizabeth R. Miller Brain Observatory | The Rockefeller University. All Rights Reserved"
 
-author = "Flynn OConnell"
+author = "Miller Brain Observatory"
 release = "1.0.0"
 
 extensions = [
